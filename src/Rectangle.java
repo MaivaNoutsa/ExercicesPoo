fc4f41f2aa90e0b2b3d8edad6701806d618cb929
zeninabou2002 <zeinaboudev@gmail.com>
2025-05-10 12:56:30 +0100

    update

--- src/Rectangle.java
+++ src/Rectangle.java
@@ -22,7 +22,7 @@ public class Rectangle {
         return pt.getX() >= p.getX() && pt.getX() <= p.getX() + width &&
                 pt.getY() >= p.getY() && pt.getY() <= p.getY() + height;
     }
-
+    // Partie zeinabou
     public boolean equals(Rectangle other) {
         return this.p.getX() == other.p.getX() && this.p.getY() == other.p.getY() &&
                 this.width == other.width && this.height == other.height;
@@ -43,4 +43,6 @@ public class Rectangle {
     public String toString() {
         return "Rectangle [p=" + p + ", width=" + width + ", height=" + height + "]";
     }
+
+    //Fin partie zeinabou
 }
